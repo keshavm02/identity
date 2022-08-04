d8ac0de8313cffd78f23536efbe903c40b5f911e
DeSoDog <ty@deso.org>
2022-08-04 13:54:22 -0500

    more testing

--- src/app/derive/derive.component.ts
+++ src/app/derive/derive.component.ts
@@ -13,7 +13,7 @@ import { GoogleDriveService } from '../google-drive.service';
 import { UserProfile } from '../../types/identity';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RouteNames } from '../app-routing.module';
-import { TransactionSpendingLimit } from 'src/lib/deso/transaction';
+import {TransactionSpendingLimit } from 'src/lib/deso/transaction';
 
 @Component({
   selector: 'app-derive',
