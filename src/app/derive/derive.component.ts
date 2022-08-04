a794d075dd2f8240a127114445ea98816142f253
DeSoDog <ty@deso.org>
2022-08-04 12:23:20 -0500

    hello

--- src/app/derive/derive.component.ts
+++ src/app/derive/derive.component.ts
@@ -3,16 +3,16 @@ import { AccountService } from '../account.service';
 import { DerivePayload, IdentityService } from '../identity.service';
 import {
   BackendAPIService,
-   CoinLimitOperationString,
+  CoinLimitOperationString,
   CoinOperationLimitMap,
   TransactionSpendingLimitResponse,
   User,
 } from '../backend-api.service';
-import {GlobalVarsService } from '../global-vars.service';
+import { GlobalVarsService } from '../global-vars.service';
 import { GoogleDriveService } from '../google-drive.service';
-import {UserProfile } from '../../types/identity';
-import {ActivatedRoute, Router } from '@angular/router';
-import { RouteNames } from '../app-routing.module';
+import { UserProfile } from '../../types/identity';
+import { ActivatedRoute, Router } from '@angular/router';
+import {RouteNames } from '../app-routing.module';
 import { TransactionSpendingLimit } from 'src/lib/deso/transaction';
 
 @Component({
